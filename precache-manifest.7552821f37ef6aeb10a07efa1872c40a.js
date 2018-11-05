91a51aec83dd306e41e6388ac412b8a2fc21c1cf
Alex Chernyakov <rustavellyne@gmail.com>
2018-11-05 14:19:56 +0200

    Updates

---
+++ precache-manifest.7552821f37ef6aeb10a07efa1872c40a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0c4e8d64904b02a0909bee104d179c",
+    "url": "/movies-search/static/media/poster_none.4c0c4e8d.png"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/movies-search/static/media/logo.27b65cb4.svg"
+  },
+  {
+    "revision": "ba39d28aa5f0e99e785e0295697f2266",
+    "url": "/movies-search/static/media/Spinner.ba39d28a.svg"
+  },
+  {
+    "revision": "935cf75022755ff080e2",
+    "url": "/movies-search/static/js/runtime~main.935cf750.js"
+  },
+  {
+    "revision": "3b4b5ae6376a44bca096",
+    "url": "/movies-search/static/js/main.3b4b5ae6.chunk.js"
+  },
+  {
+    "revision": "8f0b7c69a6e8e9ce1db1",
+    "url": "/movies-search/static/js/1.8f0b7c69.chunk.js"
+  },
+  {
+    "revision": "3b4b5ae6376a44bca096",
+    "url": "/movies-search/static/css/main.d8a608f4.chunk.css"
+  },
+  {
+    "revision": "8f0b7c69a6e8e9ce1db1",
+    "url": "/movies-search/static/css/1.c49f0430.chunk.css"
+  },
+  {
+    "revision": "6ce472e9f892efd0a737619b0744921a",
+    "url": "/movies-search/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Movie_DB/precache-manifest.155907b34fc55ff4f056a33507705938.js"
+  "/movies-search/precache-manifest.7552821f37ef6aeb10a07efa1872c40a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Movie_DB/index.html", {
+workbox.routing.registerNavigationRoute("/movies-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
