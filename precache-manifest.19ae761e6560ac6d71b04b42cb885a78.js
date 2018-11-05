32798b3a9c1c6f83b471837b8345c01d96d7b5f7
Alex Chernyakov <rustavellyne@gmail.com>
2018-11-05 13:28:27 +0200

    Updates

---
+++ precache-manifest.19ae761e6560ac6d71b04b42cb885a78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0c4e8d64904b02a0909bee104d179c",
+    "url": "/Movie_DB/static/media/poster_none.4c0c4e8d.png"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/Movie_DB/static/media/logo.27b65cb4.svg"
+  },
+  {
+    "revision": "ba39d28aa5f0e99e785e0295697f2266",
+    "url": "/Movie_DB/static/media/Spinner.ba39d28a.svg"
+  },
+  {
+    "revision": "d08d741096d2a813b2ca",
+    "url": "/Movie_DB/static/js/runtime~main.d08d7410.js"
+  },
+  {
+    "revision": "6269cdcf7dc7330fb793",
+    "url": "/Movie_DB/static/js/main.6269cdcf.chunk.js"
+  },
+  {
+    "revision": "8f0b7c69a6e8e9ce1db1",
+    "url": "/Movie_DB/static/js/1.8f0b7c69.chunk.js"
+  },
+  {
+    "revision": "6269cdcf7dc7330fb793",
+    "url": "/Movie_DB/static/css/main.50516453.chunk.css"
+  },
+  {
+    "revision": "8f0b7c69a6e8e9ce1db1",
+    "url": "/Movie_DB/static/css/1.c49f0430.chunk.css"
+  },
+  {
+    "revision": "e22c9e07cb99d6205635fbcca005a7f7",
+    "url": "/Movie_DB/index.html"
+  }
+];
